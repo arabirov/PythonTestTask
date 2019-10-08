17d51aba41318593c8b6f0a4078c050516f290e8
Alexander Rabirov <alexander.r@thrive.io>
2019-10-08 16:31:13 +0300

    Raw locator info added

--- tests/Constants/locators.py
+++ tests/Constants/locators.py
@@ -12,3 +12,27 @@ class Locators:
     # Third page
 
     # Global
+
+
+FIRST_STEP_CHECK_NECESSITY_FIELD = "#i\\.err\\.1806505028"
+FIRST_STEP_CURRENT_MONTH = "div.quantumWizTextinputPaperinputEl:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"
+FIRST_STEP_FIREFOX_SET_DAY = "div.freebirdFormviewerViewItemsDateDateSelectionInput:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"
+
+FIRST_STEP_FIREFOX_SET_MONTH = "div.freebirdFormviewerViewItemsDateDateSelectionInput:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"
+
+FIRST_STEP_FIREFOX_SET_YEAR = "div.freebirdFormviewerViewItemsDateDateSelectionInput:nth-child(5) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)"
+
+FIRST_STEP_SET_DATE = "div.quantumWizTextinputPaperinputInputArea > input"
+
+FIRST_STEP_CHECKBOX = "div.freebirdFormviewerViewNumberedItemContainer:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div"
+
+SECOND_STEP_MOVIES = "textarea.quantumWizTextinputPapertextareaInput"
+
+SECOND_STEP_RADIO_CHECK = "div.freebirdFormviewerViewNumberedItemContainer:nth-child(3) > div:nth-child(1) > div:nth-child(2) > input:nth-child(2)"
+
+BACK_BUTTON = "div.quantumWizButtonPaperbuttonEl:nth-child(1) > span:nth-child(3) > span"
+NEXT_BUTTON = "div.quantumWizButtonPaperbuttonEl:nth-child(2) > span:nth-child(3) > span"
+
+STEP_BANNER = "div.freebirdFormviewerViewItemsPagebreakBanner"
+
+RADIO_ARRAY = "span.exportContent > div:nth-child(1) > div"
