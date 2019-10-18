add222444fd4f3ffe41742f63388cc618a3a8971
Alexander Rabirov <alexander.r@thrive.io>
2019-10-18 11:37:32 +0300

    Fixes in base class

--- tests/Constants/firefox_environment.py
+++ tests/Constants/firefox_environment.py
@@ -15,15 +15,20 @@ logger.setLevel(logging.INFO)
 class Pages:
     def __init__(self, driver):
         self.driver = driver
-        self.first_page = FirstPage(self.driver)
-        self.second_page = SecondPage(self.driver)
-        self.third_page = ThirdPage(self.driver)
+
+        self.first_page = FirstPage(driver)
+        self.second_page = SecondPage(driver)
+        self.third_page = ThirdPage(driver)
 
 
-class Env(unittest.TestCase):
+class EnvironmentFirefox(unittest.TestCase, Pages):
     @classmethod
     def setUpClass(cls):
         cls.driver = webdriver.Firefox(executable_path=FIREFOX_DRIVER_PATH, service_log_path=FIREFOX_DRIVER_LOGS_PATH)
+        pages = Pages(cls.driver)
+        cls.first_page = pages.first_page
+        cls.second_page = pages.second_page
+        cls.third_page = pages.third_page
         cls.driver.implicitly_wait(3)
         cls.driver.maximize_window()
         cls.driver.get(GOOGLE_FORM_URL)
--- tests/Tests/Firefox/google_form_test.py
+++ tests/Tests/Firefox/google_form_test.py
@@ -4,7 +4,7 @@ import time
 import datetime
 import random
 
-from tests.Constants.firefox_environment import Env, Pages
+from tests.Constants.firefox_environment import EnvironmentFirefox, Pages
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -28,7 +28,7 @@ def get_random_movies():
             return movies_list
 
 
-class GoogleFormTest(Env, Pages):
+class GoogleFormTest(EnvironmentFirefox):
 
     def test_01_fill_first_and_second_questions(self):
         i = 0
